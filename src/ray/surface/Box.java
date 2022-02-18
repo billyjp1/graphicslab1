13d00abae2420bacb09c2cf6e770890dc1b79fd0
billyjp1 <billyjp@appstate.edu>
2022-02-18 17:03:51 -0500

    Passing box normals

--- src/ray/surface/Box.java
+++ src/ray/surface/Box.java
@@ -12,7 +12,23 @@ public class Box extends Surface {
 	/* The corner of the box with the largest x, y, and z components. */
 	protected final Point3 maxPt = new Point3();
 	public void setMaxPt(Point3 maxPt) { this.maxPt.set(maxPt); }
-
+	
+	double txMinus;
+	double txPlus;
+	double tyMinus;
+	double tyPlus;
+	double tzMinus;
+	double tzPlus;
+	
+	private double txMin;
+	private double txMax;
+	private double tyMin;
+	private double tyMax;
+	private double tzMin;
+	private double tzMax;
+	
+	double tEnter;
+	
 	public Box() { }
 
 	/**
@@ -21,17 +37,43 @@ public class Box extends Surface {
 	public String toString() {
 		return "box " + minPt + " " + maxPt + " " + material + " end";
 	}
-
+	
 	@Override
 	public double intersection(Vector3 ray, Point3 origin) {
 		// TODO Auto-generated method stub
+		txMinus = planeIntersection(new Vector3(ray), origin, minPt, new Vector3(-1, 0, 0));
+		txPlus = planeIntersection(new Vector3(ray), origin, maxPt, new Vector3(1, 0, 0));
 		
-		return 0;
+		tyMinus = planeIntersection(new Vector3(ray), origin, minPt, new Vector3(0, -1, 0));
+		tyPlus = planeIntersection(new Vector3(ray), origin, maxPt, new Vector3(0, 1, 0));
+		
+		tzMinus = planeIntersection(new Vector3(ray), origin, minPt, new Vector3(0, 0, -1));
+		tzPlus = planeIntersection(new Vector3(ray), origin, maxPt, new Vector3(0, 0, 1));
+		
+		txMin = Math.min(txMinus, txPlus);
+		txMax = Math.max(txMinus, txPlus);
+		
+		tyMin = Math.min(tyMinus, tyPlus);
+		tyMax = Math.max(tyMinus, tyPlus);
+		
+		tzMin = Math.min(tzMinus, tzPlus);
+		tzMax = Math.max(tzMinus, tzPlus);
+		
+		tEnter = Math.max(txMin, tyMin);
+		tEnter = Math.max(tzMin, tEnter);
+		
+		double tExit = Math.min(txMax, tyMax);
+		tExit = Math.min(tzMax, tExit);
+		
+		double t = Double.POSITIVE_INFINITY;
+		if (tEnter < tExit) {
+			t = tEnter;
+		}
+		return t;
 	}
 
-	public double planeIntersection(Vector3 ray, Point3 origin, Point3 point) {
+	public double planeIntersection(Vector3 ray, Point3 origin, Point3 point, Vector3 normal) {
 		double t = Double.POSITIVE_INFINITY;
-		Vector3 normal = getNormal(point);
 		double numer = (normal.dot(new Vector3(point))) - (normal.dot(new Vector3(origin)));
 		double denom = normal.dot(ray);
 		if (denom != 0) {
@@ -43,6 +85,41 @@ public class Box extends Surface {
 	@Override
 	public Vector3 getNormal(Point3 p) {
 		// TODO Auto-generated method stub
-		return null;
+		Vector3 normal = new Vector3();
+		if (tEnter == txMin) {
+			if (txMin == txPlus) {
+				normal = new Vector3(1,0,0);
+			} else if (txMin == txMinus) {
+				normal = new Vector3(-1,0,0);
+			}
+		} else if (tEnter == tyMin) {
+			if (tyMin == tyPlus) {
+				normal = new Vector3(0,1,0);
+			} else if (tyMin == tyMinus) {
+				normal = new Vector3(0,-1,0);
+			}
+		} else if (tEnter == tzMin) {
+			if (tzMin == tzPlus) {
+				normal = new Vector3(0,0,1);
+			} else if (txMin == txMinus) {
+				normal = new Vector3(0,0,-1);
+			}
+		}
+		
+		
+//		if (p.x == maxPt.x) {
+//			normal = new Vector3(1,0,0);
+//		} else if (p.x == minPt.x) {
+//			normal = new Vector3(-1,0,0);
+//		} else if (p.y == maxPt.y) {
+//			normal = new Vector3(0,1,0);
+//		} else if (p.y == minPt.y) {
+//			normal = new Vector3(0,-1,0);
+//		} else if (p.z == maxPt.z) {
+//			normal = new Vector3(0,0,1);
+//		} else if (p.z == minPt.z) {
+//			normal = new Vector3(0,0,-1);
+//		}
+		return normal;
 	}
 }
